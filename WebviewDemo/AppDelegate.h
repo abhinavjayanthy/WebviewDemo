58115eb9a7542a55bb030d1d124563d11463999b
ajayanthy <abhinavjayanthy@icloud.com>
2017-01-04 08:14:05 +0530

    Initial Commit

---
+++ WebviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebviewDemo
+//
+//  Created by Abhinav Jayanthy on 1/4/17.
+//  Copyright © 2017 Abhinav Jayanthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebviewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebviewDemo
+//
+//  Created by Abhinav Jayanthy on 1/4/17.
+//  Copyright © 2017 Abhinav Jayanthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
